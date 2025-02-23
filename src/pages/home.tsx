6634ac237536ba58ad5c2166310ae4eeaead895a
Dragon Messmer <dragon@skullyflower.com>
2025-02-22 19:25:14 -0800

    clean up import

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody, Center, Heading, Image, Stack, Text } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Stack, Text } from "@chakra-ui/react";
 
 function HomePage() {
   return (
